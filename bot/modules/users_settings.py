043ce67e8f67fa3378a0e6603f80e3c0b8a1a7fe
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2024-03-09 05:01:48 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -167,8 +167,9 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
 
         lremname = 'Not Exists' if (val:=user_dict.get('lremname', config_dict.get('LEECH_FILENAME_REMNAME', ''))) == '' else val
         buttons.ibutton(f"{'✅️' if lremname != 'Not Exists' else ''} Leech Remname", f"userset {user_id} lremname")
-
-        buttons.ibutton("Metadata", f"userset {user_id} metadata")
+                
+        lmetadata = 'Not Exists' if (val:=user_dict.get('lmetadata', config_dict.get('METADATA_EDIT', ''))) == '' else val
+        buttons.ibutton(f"{'✅️' if lremname != 'Not Exists' else ''} Metadata", f"userset {user_id} lmetadata")
 
         buttons.ibutton("Leech Dump", f"userset {user_id} ldump")
         ldump = 'Not Exists' if (val:=user_dict.get('ldump', '')) == '' else len(val)
